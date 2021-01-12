f7e29edfaf355861e15aa3117abe61143926ca53
nk-tamago <cqx01552@nifty.ne.jp>
2021-01-12 23:43:12 +0900

    statusとtasksのapiを追加

--- Service/web-service.js
+++ Service/web-service.js
@@ -22,20 +22,37 @@ const WebService = class {
         this.#app.get("/health", (req, res) =>{
             res.send("ok")
         })
+
         this.#app.get("/status", (req, res) =>{
-            const status = this.#tasks.map( (task)=> {
-                return { "name": task.getName(), "run" : task.isJobs()}
+            const status = this.#tasks.map( (task)=>{
+                return {"name": task.getName(), "run": task.isJobs()}
             })
-            res.send(JSON.stringify(status))
+
+            res.send(status)
+        })
+
+        this.#app.get("/status/:name", (req, res) =>{
+            const task = this.#tasks.find((t) => {
+                return (req.params.name === t.getName())
+            })
+
+            res.send((task ? {"name": task.getName(), "run": task.isJobs()} : {}))
         })
 
         this.#app.get("/tasks",  (req, res) => {
-            res.send({
-              message: "Hello world!"
+            const tasks = this.#tasks.map( (t)=>{
+                return t.toJson()
             })
+            res.send(tasks)
+        })
+
+        this.#app.get("/tasks/:name",  (req, res) => {
+            const task = this.#tasks.find((t) => {
+                return (req.params.name === t.getName())
+            })
+            res.send( task ? task.toJson(): {} )
         })
         
-        this.#app.put("/tasks")
     }
 }
 
